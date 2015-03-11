d0cdff4ad9edf3d225ba4293076d225be2bb2952
Tom Goldenberg <thomasagoldenberg@gmail.com>
2015-03-11 17:07:03 -0400

    add delete buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ActiveRecord::Base
 
   has_many :posts
   has_many :comments
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
