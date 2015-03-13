345c14fa9943a4080fae22170fd431bbda659482
Tom Goldenberg <thomasagoldenberg@gmail.com>
2015-03-12 23:56:19 -0400

    added Ajax to comment likes and dislikes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  respond_to :html, :js
   before_action :authenticate_user!
 
   def create
@@ -6,10 +7,14 @@ class CommentsController < ApplicationController
     @comment = @post.comments.create(params[:comment].permit(:comment))
     @comment.user_id = current_user.id if current_user
     @comment.save
-    if @comment.save
-      redirect_to post_path(@post)
-    else
-      render 'new'
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to post_path(@post) }
+        format.js
+      else
+        format.html { render action: 'new'}
+        format.js
+      end
     end
   end
 
@@ -34,13 +39,30 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post)
+    respond_to do |format|
+      format.html { redirect_to post_path(@post) }
+      format.js
+    end
   end
 
   def upvote
-    @comment = Comment.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
     @comment.upvote_by current_user
-    redirect_to :back
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { render json: { count: @comment.get_upvotes.size, countTwo: @comment.get_downvotes.size }}
+    end
+  end
+
+  def downvote
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.downvote_by current_user
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.json { render json: { count: @comment.get_downvotes.size, countTwo: @comment.get_upvotes.size }}
+    end
   end
 
 end
